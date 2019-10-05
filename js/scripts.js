c978ffdc797c5e82192d1e38bd4a31b9d275da23
Tobbe3108 <Tobbe3108@gmail.com>
2019-10-05 23:30:13 +0200

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+Sortable.create(A, {
+    group: "shared",
+    sort: true,
+  });
+  
+Sortable.create(B, {
+    group: "shared",
+    sort: true,
+  });
+
+Sortable.create(C, {
+    group: "shared",
+    sort: true,
+  });
+
+  Sortable.create(D, {
+    group: "shared",
+    sort: true,
+  });
